20588eaa8c2a8b842526ef957eeab57905ab016e
BOOK <mariya-chetverik@mail.ru>
2023-04-01 17:35:04 +1000

    code correction

--- home2.py
+++ home2.py
@@ -24,6 +24,6 @@ from collections import Counter
 
 def major_and_minor_elem(inp: List) -> Tuple[int, int]:
     counter = Counter(inp)  #
-    rare_letter = min(counter, key=counter.get)
-    most_common_letter = max(counter, key=counter.get)
-    return most_common_letter, rare_letter
+    least_common_element = min(counter, key=counter.get)  # Searches for the least common element
+    most_common_element = max(counter, key=counter.get)  # Searches for the most common element
+    return most_common_element, least_common_element
