3daf0c0797d1d408624ea40f0ef1972a8178cc7f
BOOK <mariya-chetverik@mail.ru>
2023-04-04 14:52:32 +1000

    code correction

--- homewk4.py
+++ homewk4.py
@@ -8,17 +8,9 @@ We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
 """
 
 from typing import List
+import collections
 
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
-    count = 0
-
-    for i in a:  # Iterating over each element in the array
-        for j in b:
-            for k in c:
-                for l in d:
-                    if i + j + k + l == 0:
-                        count += 1  # If the condition is true, then the value of the variable becomes greater than 1
-    return count
-
-
+    summ = collections.Counter(first + second for first in a for second in b) # The number and possible values of two numbers from the first and second lists.
+    return sum(summ[-third - forth] for third in c for forth in d) # All possible combinations when the sum of the four elements is zero
