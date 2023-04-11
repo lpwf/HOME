fe8f2f0ed1bf62ca94fe3e8ada409f6e7280454a
BOOK <mariya-chetverik@mail.ru>
2023-04-11 21:20:19 +1000

    code correction

--- home5.py
+++ home5.py
@@ -15,14 +15,14 @@ from typing import Any
 def custom_range(mas: str, *args: Any):
     if len(args) == 1:
         index = mas.index(args[0])
-        return mas[:index]
+        return list(mas[:index])
     elif len(args) == 2:
         start_index = mas.index(args[0])
         end_index = mas.index(args[1])
-        return mas[start_index:end_index]
+        return list(mas[start_index:end_index])
     elif len(args) > 2:
         start_index = mas.index(args[0])
         end_index = mas.index(args[1])
-        return mas[start_index:end_index:args[2]]
+        return list(mas[start_index:end_index:args[2]])
     else:
-        return mas
+        return list(mas)
