749a876f481d764f144df8c7e980548fa785a3e7
BOOK <mariya-chetverik@mail.ru>
2023-04-19 14:21:52 +1000

    done task

---
+++ hwork3.py
@@ -0,0 +1,38 @@
+"""
+Write a function that will receive a string and write it to stderr
+if line starts with "error" and to the stdout otherwise.
+
+
+>>> my_precious_logger("error: file not found")
+# stderr
+'error: file not found'
+
+
+>>> my_precious_logger("OK")
+# stdout
+'OK'
+
+Definition of done:
+ - function is created
+ - function is properly formatted
+ - function has positive tests
+
+You will learn:
+ - how to write to stderr
+ - how to test output to the stderr and stdout
+
+
+"""
+
+import sys
+
+
+def my_precious_logger(text: str):
+    if 'error' in text:
+        sys.stderr.write(text)
+    else:
+        sys.stdout.write(text)
+
+
+my_precious_logger('error gjhgjgj')
+my_precious_logger('gjhgjgj')
