718555f530d9a64948fa6c97d9841916307f983e
BOOK <mariya-chetverik@mail.ru>
2023-04-25 09:14:56 +1000

    done task

---
+++ work4.py
@@ -0,0 +1,40 @@
+import functools
+
+
+def saving_info(original_func):
+    def decorator(func):
+        func.__name__ = original_func.__name__
+        func.__doc__ = original_func.__doc__
+        func.__original_func = original_func
+        return func
+
+    return decorator
+
+
+def print_result(func):
+    @saving_info(func)
+    def wrapper(*args, **kwargs):
+        """Function-wrapper which print result of an original function"""
+        result = func(*args, **kwargs)
+        print(result)
+        return result
+
+    return wrapper
+
+
+@print_result
+def custom_sum(*args):
+    """This function can sum any objects which have __add___"""
+    return functools.reduce(lambda x, y: x + y, args)
+
+
+if __name__ == "__main__":
+    custom_sum([1, 2, 3], [4, 5])
+    custom_sum(1, 2, 3, 4)
+
+    print(custom_sum.__doc__)
+    print(custom_sum.__name__)
+    without_print = custom_sum.__original_func
+
+    # the result returns without printing
+    without_print(1, 2, 3, 4)
